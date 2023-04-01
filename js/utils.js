43df54cb94b1b6b42344c592589d6b57f07dbdfa
yuvalturjeman <120467074+yuvalturjeman@users.noreply.github.com>
2023-04-01 17:25:26 +0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -2,14 +2,18 @@ function getRandomInt(max) {
     return Math.floor(Math.random() * (max)) ;
 }
 
-function setTimer(startTime) {
-    var elapsedTime = Date.now() - startTime
-    var elTimer = document.querySelector('.timer')
-    elTimer.innerText = (elapsedTime / 1000).toFixed(3)
-}
-
-
 function startTimer() {
     var startTime = Date.now()
     gTimerInterval = setInterval(setTimer, 1, startTime)
+
+}
+
+function setTimer(startTime) {
+    gElapsedTime = Date.now() - startTime
+    renderText('.timer span', (gElapsedTime / 1000).toFixed(3))
+}
+
+function renderText(elName, value) {
+    const elSelector = document.querySelector(elName)
+    elSelector.innerText = value
 }
